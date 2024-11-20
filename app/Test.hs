a0e51ca9755fdd1d11c4a2830d10dc0bd9c6e351
DefinitelyNotRelevant <davidli6543@gmail.com>
2024-11-20 14:58:10 -0500

    Added more Item tests
    Mana potion and throwing knife tests added. Also expanded health pit tests.

--- app/Test.hs
+++ app/Test.hs
@@ -35,11 +35,37 @@ testActions = TestCase $ do
         print "p1 stamina attacking [p2, p3, p4, p5] at level 1"
         print $ staminaGroupAttack p1 [p2, p3, p4, p5] 1
 
-testItems :: Test
-testItems = TestCase $ do
+testHealthPotion :: Test
+testHealthPotion = TestCase $ do
     let p1 = Character 80 100 50 50 20 []
         p2 = makeCharacter 100 50 20
-        items = ItemList 1 1 1 1 1 1
+        p3 = Character 90 100 50 50 20 []
+        p4 = makeCharacter 100 50 20
+        items = ItemList 2 2 2 2 2 2
     print $ useHealthPotion p1 items
-    assertEqual "Health Potion Test" p1 p2
+    print $ useHealthPotion p3 items
+    assertEqual "Full Health Potion Test" p1 p2
+    assertEqual "Overflow Health Potion Test" p3 p4
+
+testManaPotion :: Test
+testManaPotion = TestCase $ do
+    let p1 = Character 100 100 30 50 20 []
+        p2 = makeCharacter 100 50 20
+        p3 = Character 100 100 40 50 20 []
+        p4 = makeCharacter 100 50 20
+        items = ItemList 2 2 2 2 2 2
+    print $ useManaPotion p1 items
+    print $ useManaPotion p3 items
+
+    assertEqual "Full Mana Potion Test" p1 p2
+    assertEqual "Overflow Mana Potion Test" p3 p4
+
+testThrowingKnives :: Test
+testThrowingKnives = TestCase $ do
+    let p1 = Character 80 100 50 50 20 []
+        p2 = makeCharacter 100 50 20
+        items = ItemList 2 2 2 2 2 2
+    print $ useThrowingKnives p2 items
+
+    assertEqual "Full Mana Potion Test" p1 p2
 
